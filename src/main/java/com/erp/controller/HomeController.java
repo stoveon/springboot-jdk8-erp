71a1257b47f4b0ea58674645422717450b69cac0
stoveon <stopluna@naver.com>
2024-09-27 18:55:24 +0900

    [ADD] init settings and home controller

---
+++ src/main/java/com/erp/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.sample.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/",method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView home(HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("home");
+        log.info("==== this is erp home root ====");
+        return mav;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: JY
+  Date: 2024-06-24
+  Time: 오후 7:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html; charset=UTF-8" language="java" pageEncoding="UTF-8" isELIgnored="false" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<c:set var="contextPath" value="${pageContext.request.contextPath}"/>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <c:out value="여긴 home.jsp 입니다" />
+</body>
+</html>
