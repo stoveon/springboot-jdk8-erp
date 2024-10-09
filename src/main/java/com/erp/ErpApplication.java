6851741d251f8c5e1fe12a6e727b1f9f57f9d2ff
stoveon <stopluna@naver.com>
2024-10-10 00:06:35 +0900

    [FIX] SpringBootApplication 실행시 에러 확인을 위한 예외 처리 및 e.printStackTrace() 추가

--- src/main/java/com/erp/ErpApplication.java
+++ src/main/java/com/erp/ErpApplication.java
@@ -2,10 +2,15 @@ package com.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ErpApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ErpApplication.class, args);
+        try {
+            SpringApplication.run(ErpApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
