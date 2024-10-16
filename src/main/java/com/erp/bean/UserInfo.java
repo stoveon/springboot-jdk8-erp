6246f4d80d114ffa42dfab12fd4ed1071e3a750a
stoveon <stopluna@naver.com>
2024-10-17 00:26:06 +0900

    [ADD] UserInfo.java, user_info_mapper.xml UserInfoMapper.java 임시 커밋 수정 필요

---
+++ src/main/java/com/erp/bean/UserInfo.java
@@ -0,0 +1,29 @@
+package com.erp.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@ToString
+public class UserInfo {
+
+    private int id;
+
+    private String loginId;
+
+    private String password;
+
+    private String name;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+}
