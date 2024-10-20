df0977b158486753ece0d45240db9cc5a999e5c7
stoveon <stopluna@naver.com>
2024-10-21 00:38:34 +0900

    [ADD] UserInfoMapper 추가

--- src/main/java/com/erp/mapper/UserInfoMapper.java
+++ src/main/java/com/erp/mapper/UserInfoMapper.java
@@ -1,8 +1,20 @@
 package com.erp.mapper;
 
+import com.erp.bean.UserInfo;
+import com.erp.bean.search.UserInfoSearch;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface UserInfoMapper {
-    String now();
+    int insertUserInfo(UserInfo userInfo);
+
+    int updateUserInfoBySid(UserInfo userInfo);
+
+    UserInfo checkExistUserInfoToLoginId(UserInfo userInfo);
+
+    List<UserInfo> selectUserInfo(UserInfoSearch search);
+
+    int selectUserInfoCount(UserInfoSearch search);
 }
