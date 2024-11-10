173abc32e6542379a9c465bd79f973521dd86865
stoveon <stopluna@naver.com>
2024-11-10 13:26:54 +0900

    [MOD] GoogleAuthUtil 수정

--- src/main/java/com/erp/util/calendar/GoogleAuthUtil.java
+++ src/main/java/com/erp/util/calendar/GoogleAuthUtil.java
@@ -1,6 +1,7 @@
 package com.erp.util.calendar;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.StoredCredential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
@@ -11,6 +12,7 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.DataStore;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
@@ -22,6 +24,7 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.*;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * <pre>
@@ -80,18 +83,24 @@ public class GoogleAuthUtil {
      */
     private static HttpTransport HTTP_TRANSPORT;
 
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("[^a-zA-Z0-9_-]");
+
     /**
      * <pre>
      *     OAuth 인증을 이용해 구글에서 인증을 가져온느 메서드
      *     - 앞에서 만든 인증 json을 불러와야 함.
      *     - 이 인증 메서드는 api 요청 때 마다 실행해서 인증정보를 반환함.
+     *
+     *     [2024.11.10] 사용자 구분 추가
+     *     - 데이터 저장소 객체를 내가 설정한 tokens/{userId}로 설정하고 GoogleAuthorizationCodeFlow 객체를 생성할 때 내가 설정한 저장소로 credential이 저장되도록 설정.
      * </pre>
      *
      * @param HTTP_TRANSPORT
+     * @param userId         : credential 파일의 이름으로 쓰일 변수
      * @return
      * @throws IOException
      */
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT, String userId) throws IOException {
         String filePath = File.separator + "static" + File.separator + "google" + File.separator + CREDENTIALS_FILE_PATH;
 
 
@@ -104,8 +113,16 @@ public class GoogleAuthUtil {
         InputStream in = classPathResource.getInputStream();
 
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        /**
+         * 이전 방식은 tokens 라는 폴더 안에 바로 "StoredCredential"이라는 파일을 생성하기 때문에 사용자별로 credential 구분이 되지 않음.
+         * 사용자를 구분해서 credential 파일을 저장하기 위해 getCredentials 함수를 다음과 같이 변경함.
+         */
+        FileDataStoreFactory dataStoreFactory = new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH));
+        DataStore<StoredCredential> dataStore = dataStoreFactory.getDataStore(getSafeUserId(userId));
+
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                .setCredentialDataStore(dataStore)
                 .setAccessType("offline")
                 .build();
 
@@ -114,6 +131,20 @@ public class GoogleAuthUtil {
 
         // returns an authorized Credential object.
         return credential;
+    }
+
+
+    /**
+     * <pre>
+     *     원래의 형식을 벗어난 단어가 있을 경우 '_'로 모두 변경해주는 메서드
+     *     - userId를 이메일로 했을 때 파일 이름에 @와 .이 들어가서 오류가 발생하는 것을 해결하기 위함.
+     * </pre>
+     *
+     * @param userId
+     * @return
+     */
+    private static String getSafeUserId(String userId) {
+        return EMAIL_PATTERN.matcher(userId).replaceAll("_");
     }
 
     /**
@@ -126,10 +157,10 @@ public class GoogleAuthUtil {
      * @param event
      * @return
      */
-    public static Event addEvent(Event event) {
+    public static Event addEvent(Event event, String userId) {
         try {
             final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT, userId))
                     .setApplicationName(APPLICATION_NAME)
                     .build();
             return service.events().insert(CALENDAR_ID, event).execute();
@@ -147,10 +178,10 @@ public class GoogleAuthUtil {
      *
      * @param eventKey
      */
-    public static void delEvent(String eventKey) {
+    public static void delEvent(String eventKey, String userId) {
         try {
             final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT, userId))
                     .setApplicationName(APPLICATION_NAME)
                     .build();
             service.events().delete(CALENDAR_ID, eventKey).execute();
@@ -161,9 +192,11 @@ public class GoogleAuthUtil {
 
     public static void main(String[] args) {
         try {
+            String userId = "stopluna97@gmail.com";
+
             // build a new authorized API client service.
             final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT, userId))
                     .setApplicationName(APPLICATION_NAME)
                     .build();
 
